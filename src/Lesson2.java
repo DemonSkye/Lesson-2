c272ad3546b83b9782482df686fe55da276704d4
DemonSkye <damien.bell.27@gmail.com>
2018-08-10 02:33:04 -0400

    .

--- src/Lesson2.java
+++ src/Lesson2.java
@@ -1,2 +1,33 @@
 public class Lesson2 {
+    public static void main(String args[]){
+        /*int numbera = 10;
+        int numberb = 5;
+        int total;
+        total = numbera * numberb;
+        // + is add, - is subtract, * is multiply, / is divide.  ^ is not exponent
+        System.out.println(total);
+
+
+        double power = 2;
+        double base = 5;
+        double result;
+        //5*5
+
+        result = Math.pow(base, power);
+        System.out.println(result);
+        */
+
+        int a = 5;
+        double b = 6.0;
+
+        //Number casting
+        int product = a*(int)b;
+        double productWithDecimal = (double)a * b;
+        System.out.println(product);
+        System.out.println(productWithDecimal);
+
+
+
+
+    }
 }
